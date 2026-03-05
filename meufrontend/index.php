e5b952ea3b8a7b95fe9f91bc62e74e21ac34e93a
wagnermarques <wagnerdocri@gmail.com>
2026-03-05 20:15:13 -0300

    transformei o html em php

---
+++ meufrontend/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade PWIII</title>
+</head>
+<body>
+    <h1>Html</h1>
+    <?php
+    $nome = "Giovanna Colaneri"
+     echo  "<h2>esse index agora é php
+     e meu nome é $nome </h2>" ?>
+</body>
+</html>
